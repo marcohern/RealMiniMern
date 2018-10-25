2b07e34ea2eaa611f02785b44243abfb2a7bd5f5
Marco Hernandez <marcohern@gmail.com>
2018-10-25 00:31:06 -0500

    redux setup

--- client/src/App.js
+++ client/src/App.js
@@ -3,25 +3,35 @@ import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
 import Login from "./components/auth/Login";
-import Register from "./components/auth/Register";
-
+import UserList from "./components/user/UserList";
+import UserForm from "./components/user/UserForm";
+import { Provider } from "react-redux";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import logo from "./logo.svg";
 import "./App.css";
+import store from "./store";
 
 class App extends Component {
+  constructor() {
+    super();
+  }
+
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Navbar />
-          <Route exact path="/" component={Landing} />
-          <div className="container">
-            <Route exact path="/login" component={Login} />
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Route exact path="/" component={Landing} />
+            <div className="container">
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/users" component={UserList} />
+            </div>
+            <Footer />
           </div>
-          <Footer />
-        </div>
-      </Router>
+        </Router>
+      </Provider>
     );
   }
 }
